d1ac39d9e0f5ddad1ba39db14583a5d47eebc076
Loreley <mail@lory.dev>
2022-06-05 02:16:26 +0200

    fixed scroll list overflow bug

--- renderer/components/sidebar/sidebar_viewWrapper.tsx
+++ renderer/components/sidebar/sidebar_viewWrapper.tsx
@@ -2,11 +2,13 @@ import {FunctionComponent, ReactElement} from "react";
 
 const Sidebar_ViewWrapper = ({title, children}) => {
     return (
-        <div className={"w-full"}>
-            <div className={"p-3 py-3 border-b dark:border-gray-500"}>
+        <div className={"w-full relative h-full"}>
+            <div className={"p-3 py-3 h-16 border-b dark:border-gray-500"}>
                 <h1 className={"text-3xl font-bold"}>{title}</h1>
             </div>
-            {children}
+            <div className={"sidebarWrapperChildrenHeight"}>
+                {children}
+            </div>
         </div>
     )
 
